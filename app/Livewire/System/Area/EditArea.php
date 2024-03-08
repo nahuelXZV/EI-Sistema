5338be8d7adbf7f1d1e07153f66e7b9ede635597
Daniela Carrasco <90220457+dalbacarrasco@users.noreply.github.com>
2024-03-07 22:02:28 -0400

    add(area): Add crud area

--- app/Livewire/System/Area/EditArea.php
+++ app/Livewire/System/Area/EditArea.php
@@ -2,10 +2,39 @@
 
 namespace App\Livewire\System\Area;
 
+use App\Services\System\AreaService;
 use Livewire\Component;
 
 class EditArea extends Component
 {
+    public $breadcrumbs = [['title' => "Areas", "url" => "area.list"], ['title' => "Crear", "url" => "area.create"]];
+    public $areaArray = [];
+
+    public $validate = [
+        'areaArray.nombre' => 'required',
+
+    ];
+
+    public $message = [
+        'areaArray.nombre.required' => 'El nombre es requerido',
+    ];
+
+    public function mount($area)
+    {
+        $areaEntity = AreaService::getOne($area);
+        $this->areaArray = [
+            'id' => $areaEntity->id,
+            'nombre' => $areaEntity->nombre,
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate($this->validate, $this->message);
+        AreaService::update($this->areaArray);
+        return redirect()->route('area.list');
+    }
+
     public function render()
     {
         return view('livewire.system.area.edit-area');
