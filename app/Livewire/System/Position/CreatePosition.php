1d1466788bb618480a5e79fa58cbafb2adcb1c44
Daniela Carrasco <90220457+dalbacarrasco@users.noreply.github.com>
2024-03-11 10:13:48 -0400

    add(position): Add crud position

--- app/Livewire/System/Position/CreatePosition.php
+++ app/Livewire/System/Position/CreatePosition.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire\System\Position;
 
+use App\Services\System\PositionService;
 use Livewire\Component;
 
 class CreatePosition extends Component
 {
+    public $breadcrumbs = [['title' => "Cargos", "url" => "position.list"], ['title' => "Crear", "url" => "position.create"]];
+    public $positionArray = [];
+
+    public $validate = [
+        'positionArray.nombre' => 'required',
+    ];
+
+    public $message = [
+        'positionArray.nombre.required' => 'El nombre es requerido',
+    ];
+
+    public function mount()
+    {
+        $this->positionArray = [
+            'nombre' => '',
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate($this->validate, $this->message);
+        PositionService::create($this->positionArray);
+        return redirect()->route('position.list');
+    }
+
     public function render()
     {
         return view('livewire.system.position.create-position');
--- app/Livewire/System/Position/EditPosition.php
+++ app/Livewire/System/Position/EditPosition.php
@@ -2,10 +2,40 @@
 
 namespace App\Livewire\System\Position;
 
+use App\Services\System\PositionService;
 use Livewire\Component;
 
 class EditPosition extends Component
 {
+    public $breadcrumbs = [['title' => "Cargos", "url" => "position.list"], ['title' => "Crear", "url" => "position.create"]];
+    public $positionArray = [];
+
+    public $validate = [
+        'positionArray.nombre' => 'required',
+
+    ];
+
+    public $message = [
+        'positionArray.nombre.required' => 'El nombre es requerido',
+    ];
+
+    public function mount($position)
+    {
+        $positionEntity = PositionService::getOne($position);
+        $this->positionArray = [
+            'id' => $positionEntity->id,
+            'nombre' => $positionEntity->nombre,
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate($this->validate, $this->message);
+        PositionService::update($this->positionArray);
+        return redirect()->route('position.list');
+    }
+
+
     public function render()
     {
         return view('livewire.system.position.edit-position');
