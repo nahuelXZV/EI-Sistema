e47108315d856d84f6ddfe8a841ca1a57c1658e6
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-10-20 15:52:27 -0400

    fix(letter): Fix download letter

--- app/Letters/Teachers/TerminoReferencia.php
+++ app/Letters/Teachers/TerminoReferencia.php
@@ -29,7 +29,9 @@ class TerminoReferencia extends FpdfGlobal implements LetterDownloadInterface
         $program = ProgramService::getOne($module->programa_id);
 
         $letterLeaders = LetterLeader::where('letter_id', $letterId)->first();
+        dd($letterLeaders);
         if (!$letterLeaders) return false;
+        dd('here');
         $leader = Leader::find($letterLeaders->leader_id);
         $fullnameLeader = $this->getFullNameLeader($leader) ?? "No asignado";
 
