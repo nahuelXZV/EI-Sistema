fa14b75f77f1d0b328cc3cfad5c76d12921dde66
Nahuel Zalazar <zvnahuel63@gmail.com>
2024-11-03 14:17:34 -0400

    add(contract): add edit contract

--- app/Livewire/Academic/Contract/EditContract.php
+++ app/Livewire/Academic/Contract/EditContract.php
@@ -2,10 +2,70 @@
 
 namespace App\Livewire\Academic\Contract;
 
+use App\Services\Academic\ContractService;
+use App\Services\Academic\CourseService;
+use App\Services\Academic\ModuleService;
+use App\Services\Academic\TeacherService;
 use Livewire\Component;
 
 class EditContract extends Component
 {
+    public $breadcrumbs;
+    public $teacher;
+    public $contract;
+
+    public $contractArray = [];
+    public $modulos = [];
+    public $cursos = [];
+
+    public $validate = [
+        'contractArray.honorarios' => 'required',
+        'contractArray.fecha_inicio' => 'required',
+        'contractArray.fecha_fin' => 'required',
+        'contractArray.horario' => 'required',
+        'contractArray.nro_preventiva' => 'required',
+    ];
+
+    public $message = [
+        'contractArray.honorarios.required' => 'El honorario es requerido',
+        'contractArray.fecha_inicio.required' => 'La fecha de inicio es requerida',
+        'contractArray.fecha_fin.required' => 'La fecha de fin es requerida',
+        'contractArray.horario.required' => 'El horario es requerido',
+        'contractArray.nro_preventiva.required' => 'El número de preventiva es requerido',
+    ];
+
+    public function mount($contract)
+    {
+        $this->contract = ContractService::getOne($contract);
+        $this->teacher = TeacherService::getOne($this->contract->docente_id);
+        $this->modulos = ModuleService::getAllWithoutContract($this->contract->docente_id);
+        $this->cursos = CourseService::getAllWithoutContract($this->contract->docente_id);
+        $nameTeacher = $this->teacher->nombre . ' ' . $this->teacher->apellido;
+        $this->breadcrumbs = [
+            ['title' => "Docentes", "url" => "teacher.list"], // docente
+            ['title' => $nameTeacher, "url" => "teacher.show", "id" => $this->contract->docente_id], // docente
+            ['title' => "Contrato", "url" => "contract.new", "id" => $this->contract->docente_id], // contrato
+            ['title' => "Crear", "url" => "contract.new", "id" => $this->contract->docente_id] // contrato
+        ];
+        $this->contractArray = [
+            'id' => $this->contract->id,
+            'honorarios' => $this->contract->honorarios,
+            'fecha_inicio' => $this->contract->fecha_inicio,
+            'fecha_fin' =>  $this->contract->fecha_fin,
+            'horario' => $this->contract->horario,
+            'nro_preventiva' => $this->contract->nro_preventiva,
+            'modulo_id' => $this->contract->modulo_id,
+            'curso_id' => $this->contract->curso_id,
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate($this->validate, $this->message);
+        ContractService::update($this->contractArray);
+        return redirect()->route('teacher.show', $this->teacher->id);
+    }
+
     public function render()
     {
         return view('livewire.academic.contract.edit-contract');
--- resources/views/livewire/academic/contract/edit-contract.blade.php
+++ resources/views/livewire/academic/contract/edit-contract.blade.php
@@ -1,3 +1,82 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <x-shared.container :breadcrumbs="$breadcrumbs">
+        <div class="relative overflow-hidden bg-white  dark:bg-gray-800 sm:rounded-lg">
+            <div class="flex-row items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4">
+                <div>
+                    <h5 class="mr-3 text-lg font-bold dark:text-white uppercase">Editar</h5>
+                </div>
+
+                <div class="flex items-center space-x-3">
+                    <x-shared.button-header title="Volver" route="teacher.show" :params="[$teacher->id]" />
+                    <button wire:click="save" type="button"
+                        class="w-min flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-fondo hover:bg-primary-900 focus:ring-4 focus:ring-fondo dark:bg-fondo dark:hover:bg-primary-900 focus:outline-none dark:focus:ring-primary-800">
+                        Guardar
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="max-w px-4 py-8 mx-auto">
+            <section>
+                <div class="grid gap-4 mb-4 sm:grid-cols-3 sm:gap-6 sm:mb-5">
+                    <div class="col-span-3 sm:col-span-1">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Honorario
+                        </label>
+                        <input type="number" wire:model="contractArray.honorarios"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Honorario">
+                        @error('contractArray.honorarios')
+                            <x-shared.validate-error :message="$message" />
+                        @enderror
+                    </div>
+                    <div class="col-span-3 sm:col-span-1">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha Inicio
+                        </label>
+                        <input type="date" wire:model="contractArray.fecha_inicio"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        @error('contractArray.fecha_inicio')
+                            <x-shared.validate-error :message="$message" />
+                        @enderror
+                    </div>
+
+                    <div class="col-span-3 sm:col-span-1">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha Fin
+                        </label>
+                        <input type="date" wire:model="contractArray.fecha_fin"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        @error('contractArray.fecha_fin')
+                            <x-shared.validate-error :message="$message" />
+                        @enderror
+                    </div>
+
+                    <div class="col-span-3 sm:col-span-1">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numero preventiva
+                        </label>
+                        <input type="text" wire:model="contractArray.nro_preventiva"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Numero preventiva">
+                        @error('contractArray.nro_preventiva')
+                            <x-shared.validate-error :message="$message" />
+                        @enderror
+                    </div>
+                    <x-shared.space />
+                    <x-shared.space />
+
+                    <div class="col-span-3 sm:col-span-2">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Horario
+                        </label>
+                        <textarea rows="4" wire:model.blur="contractArray.horario"
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Horario"></textarea>
+                        @error('contractArray.horario')
+                            <x-shared.validate-error :message="$message" />
+                        @enderror
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    </x-shared.container>
 </div>
